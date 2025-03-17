126ed397fb0edc79ded754adbb8ac9352042ff3b
paola-code <paola.ale.pena@gmail.com>
2025-03-16 21:58:05 -0300

    logica inicial desafio

---
+++ app.js
@@ -0,0 +1,37 @@
+let listaAmigos = [];
+
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value.trim()
+    if (nombreAmigo == ""){
+        alert("Debe ingresar un nombre")
+    } else {
+        listaAmigos.push(nombreAmigo)
+        document.getElementById('amigo').value = ""
+        mostrarAmigos()
+    }
+}
+  
+function mostrarAmigos(){
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = ""
+    for(let i = 0; i < listaAmigos.length; i++){
+        let item = document.createElement('li')
+        item.textContent = listaAmigos[i]
+        lista.appendChild(item)
+    }
+}
+
+function sortearAmigo(){
+    let ganador = listaAmigos[Math.floor(Math.random() * listaAmigos.length)]
+    limpiarLista()
+    let resultado =  document.getElementById('resultado')
+    resultado.style.color = 'green'
+    resultado.textContent = `El amigo sorteado es: ${ganador}`
+    
+}
+
+function limpiarLista(){
+    listaAmigos = []
+    mostrarAmigos()
+}
